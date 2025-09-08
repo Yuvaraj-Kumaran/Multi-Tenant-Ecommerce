95ebc4b598c85e0e9e161cac09c226e797fe3356
Yuvaraj Ajay <yuvaraj-kumaran@users.noreply.github.com>
2025-09-08 07:34:07 +0530

    customization of UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <>
-        <p className="text-rose-500">
-          Hello World!
-        </p>
-    </>
+  <div className="p-5">
+   <div className="flex flex-col gap-y-4">  
+      <div>
+        <Button variant="elevated">
+          Button 
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="Input"></Input>
+      </div>
+      <div>
+        <Progress value={50}></Progress>
+      </div>
+      <div>
+        <Textarea placeholder="text AREA"/>
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+   </div>
+  </div>
   )
 }
