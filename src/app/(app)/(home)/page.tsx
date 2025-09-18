d4158b46642e10febe9b379633addc78fd3ee94f
Yuvaraj Ajay <yuvarajk0817@gmail.com>
2025-09-18 07:33:43 +0530

    integrated payload

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,45 @@
+// import { Button } from "@/components/ui/button";
+// import { Checkbox } from "@/components/ui/checkbox";
+// import { Input } from "@/components/ui/input";
+// import { Progress } from "@/components/ui/progress";
+// import { Textarea } from "@/components/ui/textarea";
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+export default async function Home() {
+   const payload = await getPayload({
+      config: configPromise,
+    })
+
+  const data = await payload.find({
+    collection: "categories",
+  })  
+  return (
+  // <div className="p-5">
+  //  <div className="flex flex-col gap-y-4">  
+  //     <div>
+  //       <Button variant="elevated">
+  //         Button 
+  //       </Button>
+  //     </div>
+  //     <div>
+  //       <Input placeholder="Input"></Input>
+  //     </div>
+  //     <div>
+  //       <Progress value={50}></Progress>
+  //     </div>
+  //     <div>
+  //       <Textarea placeholder="text AREA"/>
+  //     </div>
+  //     <div>
+  //       <Checkbox />
+  //     </div>
+  //  </div>
+  // </div>
+  
+  <div>
+    {
+      JSON.stringify(data, null, 2)
+    }
+  </div>
+  )
+}
